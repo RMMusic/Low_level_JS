13c0a7f61493f35a055b015d95a0b1d87e8842c4
RMMusic <r.m.alien.ua@gmail.com>
2016-03-09 02:51:56 +0200

    Events 09.03.16

---
+++ events/script.js
@@ -0,0 +1,44 @@
+/**
+ * Created by admin on 09.03.2016.
+ */
+$( document ).ready(function() {
+    $('body').append("<p style='font-size: 24pt'>.ready()<p>");
+    $('#clickA').on('click', function( event ) {
+        event.preventDefault();
+        $(this).find("#clickA1").attr('src',"4.jpg");
+    });
+    //$('#clickS').on('click', function() {
+    //    $(this).attr('src',"4.jpg");
+    //});
+
+    $("#clickS").mousedown(function(){
+        $(this).attr('src',"4.jpg");
+    }).mouseup(function(){
+        $(this).attr('src',"3.jpg");
+    });
+
+    $( "#target" ).on('keydown', function() {
+        $(this).css({background: "#97F9A7"});
+        $('body').append("<p style='font-size: 24pt'>Background color changed<p>");
+    });
+
+    //Ask a question about this shit
+    $("span").hover(
+        function () {
+            $(this).css({color: "blue"});
+        },
+        function () {
+            $(this).css({color: ""});
+        }
+    );
+    $("span#hover").hover(
+        function () {
+            $(this).css({color: "red"});
+        },
+        function () {
+            $(this).css({color: ""});
+        }
+    );
+    //end shit
+});
+
