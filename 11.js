90f9bbbe4c46c8a62c0bb136981123c582b542bd
RMMusic <r.m.alien.ua@gmail.com>
2016-03-09 09:56:49 +0200

    work

---
+++ 11.js
@@ -0,0 +1,310 @@
+var data = {
+    "Conference": {
+        "IsMuteAll": "0",
+        "IsMeetingLocked": "1",
+        "Recording":"NOT_RECORDING",
+        "IsTagging": "0",
+        "CurrentPreset": {
+            "1": {
+                "preset": "preset1",
+                "csum": "423434321"
+            },
+            "2": {
+                "preset": "preset2",
+                "csum": "423434321"
+            },
+            "3": {
+                "preset": "preset1",
+                "csum": "423434321"
+            }
+        },
+        "ParticipantList": [
+
+            {
+                "DisplayName": "Mondocenter",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "address": "83645@infocus.net",
+            "id": "1",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Participant"
+            },
+            {
+                "DisplayName": "sdfsdfsdf",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "34",
+                "address": "345345@infocus.net",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Participant"
+            },
+            {
+                "DisplayName": "sdfsdfsdf",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "2",
+                "address": "456456@infocus.net",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Participant"
+            },
+            {
+                "DisplayName": "asdasda",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "3",
+                "address": "35345@infocus.net",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Participant"
+            },
+            {
+                "DisplayName": "fsdgsdf",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "4",
+                "address": "2343@infocus.net",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Local"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "5",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "6",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "7",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "9",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "99",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "68",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": 0 ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "78",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "98",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            },
+            {
+                "DisplayName": "asdasd",
+                "StartTime": "08/05/2015 10:11:17 PM GMT",
+                "MediaType": "AUDIO_AND_PRIMARY_VIDEO",
+                "id": "998",
+                "Capabilities": {
+                    "Broadcast": {
+                        "IsBroadcaster": "1"
+                    },
+                    "StartStop": "0" ,
+                    "Microphone": {
+                        "IsMute": "0"
+                    },
+                    "End": {},
+                    "Label": {},
+                    "Window": {}
+                },
+                "Window": {},
+                "ParticipantType": "Presentation"
+            }
+        ]
+    }
+}
